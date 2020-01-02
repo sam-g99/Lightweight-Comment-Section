fd06e5211505150aaac7c0835b2559bab3e4a577
Sam G <46730010+sam-g99@users.noreply.github.com>
2020-01-02 08:03:31 -0500

    webpack and linting

---
+++ src/index.js
@@ -0,0 +1,118 @@
+
+import './main.css';
+
+const ta = {
+  placeholder: 'Type here to comment...',
+  maxLength: 500,
+};
+
+// Creating Elements
+const commentRoot = document.getElementById('commentRoot');
+const rootFragment = new DocumentFragment();
+
+const createTextarea = () => {
+  const charsLeftDisplay = (ta.maxLength) ? `<div id="charsLeftDisplay" " class="characters-left">${ta.maxLength}</div>` : '';
+
+  return `
+        <div class="comment-box-container">
+            <textarea id="commentBox" maxlength="${ta.maxLength}" placeholder="${ta.placeholder}"></textarea>
+            ${charsLeftDisplay}
+        </div>
+    `;
+};
+
+const createCommentButton = () => '<button id="commentButton"> Comment </button>';
+
+const createCommentBoxArea = () => {
+  const commentBoxArea = document.createElement('div');
+  commentBoxArea.setAttribute('class', 'textarea-container');
+
+  const textarea = createTextarea(ta.placeholder, ta.maxLength);
+  const button = createCommentButton();
+
+  const elements = [textarea, button];
+
+  // Add comment box area elements
+  elements.forEach((el) => {
+    commentBoxArea.insertAdjacentHTML('beforeend', el);
+  });
+
+
+  rootFragment.appendChild(commentBoxArea);
+};
+
+
+// Comment Logic
+class Comment {
+  constructor(author, content, isReply) {
+    this.author = author;
+    this.content = content;
+    this.isReply = isReply;
+  }
+
+  createElement() {
+    const comment = `<div><strong>${this.author}</strong></div><div id="comment"></div><br>`;
+    const commentFragment = document.createRange().createContextualFragment(comment);
+    commentFragment.getElementById('comment').textContent = this.content;
+    return commentFragment;
+  }
+}
+
+const comments = [];
+
+comments.push(new Comment('Bob', 'I hate pickles', false));
+comments.push(new Comment('Jane', 'I love tacos', false));
+
+const createPageCommentsArea = () => {
+  const pageCommentArea = document.createElement('div');
+  pageCommentArea.setAttribute('class', 'page-comments');
+  pageCommentArea.setAttribute('id', 'pageCommentArea');
+
+  comments.forEach((c) => {
+    pageCommentArea.appendChild(c.createElement());
+  });
+
+  rootFragment.appendChild(pageCommentArea);
+};
+
+// Render Comment Section
+(function render() {
+  createCommentBoxArea();
+  createPageCommentsArea();
+  commentRoot.appendChild(rootFragment);
+}());
+
+// Action Functions
+const postComment = () => {
+  const content = document.getElementById('commentBox').value;
+  const comment = new Comment('Joe', content, false);
+  comments.push(comment);
+  document.getElementById('pageCommentArea').appendChild(comment.createElement());
+};
+
+
+// Adding Events To Textarea
+const commentBox = document.getElementById('commentBox');
+const commentButton = document.getElementById('commentButton');
+
+commentBox.addEventListener('focus', () => {
+  console.log('Comment box focused.');
+});
+
+commentBox.addEventListener('blur', () => {
+  console.log('Comment box blurred.');
+});
+
+commentButton.addEventListener('mouseup', postComment, false);
+
+// Show how many characters the user has left
+const charsLeft = (e) => {
+  const textarea = e.target;
+  const charsLeftNum = ta.maxLength - textarea.value.length;
+  document.getElementById('charsLeftDisplay').textContent = charsLeftNum;
+};
+
+if (ta.maxLength) {
+  commentBox.addEventListener('keydown', charsLeft, false);
+  commentBox.addEventListener('keyup', charsLeft, false);
+}
