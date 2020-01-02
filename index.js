495ca0303111ca899fe48089b46fa87bf29fb853
Sam G <46730010+sam-g99@users.noreply.github.com>
2020-01-02 03:33:14 -0500

    document fragment

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let ta = {
 
 // Creating Elements
 const commentRoot = document.getElementById('commentRoot');
+let rootFragment = new DocumentFragment();
 
 const createTextarea = () => {
     let charsLeftDisplay = (ta.maxLength) ? `<div id="charsLeftDisplay" " class="characters-left">${ta.maxLength}</div>` : '';
@@ -20,7 +21,7 @@ const createTextarea = () => {
 }
 
 const createCommentButton = () => {
-    return `<button> Comment </button>`;
+    return `<button id="commentButton"> Comment </button>`;
 }
 
 const createCommentBoxArea = () => {
@@ -33,11 +34,12 @@ const createCommentBoxArea = () => {
     const elements = [textarea, button]
 
     // Add comment box area elements
-    elements.forEach((el) => {
+    for (let el of elements) {
         commentBoxArea.insertAdjacentHTML('beforeend', el.trim());
-    })
+    }
     
-    commentRoot.appendChild(commentBoxArea);
+    rootFragment.appendChild(commentBoxArea);
+    console.log(rootFragment);
 }
 
 // Comment Logic
@@ -54,11 +56,13 @@ console.log(new Comment('Bob', 'I hate pickles', false));
 // Render Comment Section
 (function render() {
     createCommentBoxArea();
+    commentRoot.appendChild(rootFragment);
 })()
 
 
 // Adding Events To Textarea
 const commentBox = document.getElementById('commentBox');
+const commentButton = document.getElementById('commentButton');
 
 commentBox.addEventListener('focus', () => {
     console.log('Comment box focused.');
@@ -66,6 +70,10 @@ commentBox.addEventListener('focus', () => {
 
 commentBox.addEventListener('blur', () => {
     console.log('Comment box blurred.');
+})
+
+commentButton.addEventListener('mouseup', () => {
+    console.log('Button clicked.')
 })
 
 const charsLeft = (e) => {
